1a062124ea5d06dd4824535e89acb06e72986254
Navneet Gupta <Navneet-gupta01@users.noreply.github.com>
2017-10-05 06:17:38 +0400

    changes

--- src/fp/chapter2/exercises/Exc2IsSorted.scala
+++ src/fp/chapter2/exercises/Exc2IsSorted.scala
@@ -10,31 +10,31 @@ object Exc2IsSorted {
         if(start>= arr.length-1) false
         else {
           val greater = comparator(arr(start),arr(start+1))
-          
+
         }
       }
     }
     false
   }*/
   // this checks only if sorted in ascending order
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
-    @annotation.tailrec
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @tailrec
     def go(n: Int): Boolean =
-      if (n >= as.length-1) true
-      else if (gt(as(n), as(n+1))) false
-      else go(n+1)
-  
+      if (n >= as.length - 1) true
+      else if (gt(as(n), as(n + 1))) false
+      else go(n + 1)
+
     go(0)
   }
   //to check both sorted array Ascending or Descending
-  def isSortedBoth[A](xs:Array[A],comparator:(A,A) => Boolean): Boolean = {
-    @annotation.tailrec
-    def check(n:Int,isAsc:Boolean) :Boolean = 
-      if(n>= xs.length-1) true
-      else if(comparator(xs(n),xs(n+1)) && isAsc) false
-      else if(comparator(xs(n+1),xs(n)) && !isAsc) false
-      else check(n+1,isAsc)
-      
-    check(0,comparator(xs(1),xs(0)))
+  def isSortedBoth[A](xs: Array[A], comparator: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def check(n: Int, isAsc: Boolean): Boolean =
+      if (n >= xs.length - 1) true
+      else if (comparator(xs(n), xs(n + 1)) && isAsc) false
+      else if (comparator(xs(n + 1), xs(n)) && !isAsc) false
+      else check(n + 1, isAsc)
+
+    check(0, comparator(xs(1), xs(0)))
   }
 }
