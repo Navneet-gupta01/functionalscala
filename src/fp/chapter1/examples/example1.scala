cff270f2f69556dc0a4c3789ec7b7463aaac4764
Navneet <navneet.gupta.iitbhu@gmail.com>
2018-07-26 10:30:20 +0400

    changes

--- src/fp/chapter1/examples/example1.scala
+++ src/fp/chapter1/examples/example1.scala
@@ -1,26 +1,26 @@
 package fp.chapter1.examples
 
 object example1 {
-  case class Player(name: String,score : Int)
-  
-  def printWinner(p: Player) : Unit = {
+  case class Player(name: String, score: Int)
+
+  def printWinner(p: Player): Unit = {
     println(p.name + "  is winner !!");
   }
-  
-  def declareWinner(p1: Player, p2 :Player): Unit = {
-    if(p1.score > p2.score) {
+
+  def declareWinner(p1: Player, p2: Player): Unit = {
+    if (p1.score > p2.score) {
       printWinner(p1)
-    } else 
+    } else
       printWinner(p2)
   }
-  
+
   // now refactor above code as below
-  
-  def winner(p1 : Player,p2:Player) : Player = {
-    if(p1.score > p2.score) p1 else p2
+  //pure function
+  def winner(p1: Player, p2: Player): Player = {
+    if (p1.score > p2.score) p1 else p2
   }
-  
-  def declareWinnerRefactored(p1:Player,p2:Player): Unit = {
-    printWinner(winner(p1,p2))
+  //impure function
+  def declareWinnerRefactored(p1: Player, p2: Player): Unit = {
+    printWinner(winner(p1, p2))
   }
 }
